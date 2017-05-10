36a333fadcf0b443bc570a666d0a71722399e7d1
Tom Carchrae <tom@intellecti.ca>
2017-05-10 16:37:51 -0700

    use rails 4 property `secret_key_base`

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,11 +1,11 @@
-
-Rails.logger.warn('no secret token set') if ENV['SECRET'].blank?
+secret = FoodsoftConfig[:secret] || ENV['SECRET'] || ENV['SECRET_KEY_BASE']
+Rails.logger.warn('no secret token set') if secret.blank?
 
 if Rails.env ==='production'
-  Foodsoft::Application.config.secret_token=ENV['SECRET']
-  raise 'no environment variable SECRET defined, generate one with: openssl rand -hex 128' unless ENV['SECRET']
+  Foodsoft::Application.config.secret_key_base=secret
+  raise 'no environment variable SECRET defined, generate one with: openssl rand -hex 128' unless secret
 else
-  Foodsoft::Application.config.secret_token=ENV['SECRET'] || 'NO_SECRET'
+  Foodsoft::Application.config.secret_key_base=secret || 'you really really need to change me! you really really need to change me! development only'
 end
 
 
