9d0815ef04b22def00e400e5857dc33eeca5a161
Tom Carchrae <tom@intellecti.ca>
2017-05-12 17:26:16 -0700

    better default for redis in production (stops it crashing if redis not defined)

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,8 @@
 # Initializer to configure resque daemon
-Resque.redis = ENV['REDIS_URL'] if ENV['REDIS_URL']
+if ENV['REDIS_URL']
+  #ensure you are also running the worker - rake resque:work QUEUE=foodsoft_notifier
+  Resque.redis = ENV['REDIS_URL']
+else
+  puts 'Warning, no REDIS_URL defined, using inline'
+  Resque.inline = true
+end
