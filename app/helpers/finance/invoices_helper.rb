db594f511043d6bad63ecdff882162e38c59a4b5
Tom Carchrae <tom@intellecti.ca>
2017-06-02 06:26:37 -0700

    trying to fix a crash when order passed in was ["Yes", true]:Array instead of a valid order

--- app/helpers/finance/invoices_helper.rb
+++ app/helpers/finance/invoices_helper.rb
@@ -3,6 +3,11 @@ module Finance::InvoicesHelper
     format_date(delivery.delivered_on)
   end
   def format_order_item order
-    "#{format_date(order.ends)} (#{number_to_currency(order.sum)})"
+    begin
+      "#{format_date(order.ends)} (#{number_to_currency(order.sum)})"
+    rescue => e
+      Rails.logger.error("format_order_item of order=#{order.inspect} failed: #{e.message}")
+      '?'
+    end
   end
 end
