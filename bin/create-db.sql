fde77a479799b66b2b4721407ccd25846f1f956e
Tom Carchrae <tom@intellecti.ca>
2018-12-14 15:37:19 -0800

    commit db setup utils

---
+++ bin/create-db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE "food-coop";
+CREATE USER "food-coop" WITH ENCRYPTED PASSWORD 'food-coop';
+GRANT ALL PRIVILEGES ON DATABASE "food-coop" TO "food-coop";
---
+++ bin/docker-db.sh
@@ -0,0 +1,2 @@
+
+docker run --name local-postgres -p5432:5432 -e POSTGRES_PASSWORD=root postgres:9.5
